636fdc9f90bfefe75023c39f3c968bc5176dda9c
Litman <litman_ayala@hotmail.com>
2017-12-21 10:25:43 -0500

    Change to add more specifications

--- src/main/java/com/ghostl/tddjava/gradle/TicTacToe.java
+++ src/main/java/com/ghostl/tddjava/gradle/TicTacToe.java
@@ -1,31 +1,60 @@
 package com.ghostl.tddjava.gradle;
 
+import java.net.UnknownHostException;
+
+import com.ghostl.tddjava.beans.TicTacToeBean;
+import com.ghostl.tddjava.mongo.TicTacToeCollection;
+
 public class TicTacToe {
 
 	private Character[][] board = { { '\0', '\0', '\0' }, { '\0', '\0', '\0' }, { '\0', '\0', '\0' } };
 	private char lastPlayer = '\0';
-	private static final int SIZE =3;
+	
+	private int turn = 0;
 
+	private String RESULT_DRAW = "The result is draw";
+	private String NO_WINNER = "No Winner";
+	
+	private static final int SIZE = 3;
+	private TicTacToeCollection ticTacToeCollection;
+	
+	public TicTacToe() throws UnknownHostException {
+	    this(new TicTacToeCollection());
+	}
+	protected TicTacToe (TicTacToeCollection collection) {
+	    ticTacToeCollection = collection;
+	}
+	
+	public TicTacToeCollection getTicTacToeCollection() {
+		return ticTacToeCollection;
+	}
+
+
+	public void setTicTacToeCollection(TicTacToeCollection ticTacToeCollection) {
+		this.ticTacToeCollection = ticTacToeCollection;
+	}
+	
+	
 	public String play(int x, int y) {
 
 		checkAxis(x);
 		checkAxis(y);
 		lastPlayer = nextPlayer();
-		setBox(x, y, lastPlayer);
-		
-		if(isWin()){
+		//setBox(x, y, lastPlayer);
+		setBox(new TicTacToeBean(++turn, x, y, lastPlayer));
+		if (isWin(x, y)) {
 			return lastPlayer + " is the Winner!!";
-		}else if(isDraw()){
-			return "The result is draw";
+		} else if (isDraw()) {
+			return RESULT_DRAW;//"The result is draw";
 		}
-		return "No Winner";
+		return NO_WINNER;//"No Winner";
 
 	}
-	
+
 	private boolean isDraw() {
-		for(int x=0; x < SIZE; x++){
+		for (int x = 0; x < SIZE; x++) {
 			for (int y = 0; y < SIZE; y++) {
-				if(board[x][y] == '\0'){
+				if (board[x][y] == '\0') {
 					return false;
 				}
 			}
@@ -33,40 +62,42 @@ public class TicTacToe {
 		return true;
 	}
 
-	private boolean isWin(){
-		int playerTotal = lastPlayer*3;
-		char diagonal1 = '\0';
-		char diagonal2 = '\0';
-		for(int i=0; i<SIZE; i++){
+	private boolean isWin(int x, int y) {
+		int playerTotal = lastPlayer * 3;
+		char diagonal1, diagonal2, vertical, horizontal;
+		diagonal1 = diagonal2 = vertical = horizontal = '\0';
+		for (int i = 0; i < SIZE; i++) {
+			horizontal += board[i][y - 1];
+			vertical += board[x - 1][i];
 			diagonal1 += board[i][i];
 			diagonal2 += board[i][SIZE - i - 1];
-			if(board[0][i] + board[1][i] + board[2][i] == playerTotal){
-				return true;
-			}
-			if(board[i][0] + board[i][1] + board[i][2] == playerTotal){
-				return true;
-			}
-			if(diagonal1 == playerTotal || diagonal2 == playerTotal){
+			if (horizontal == playerTotal 
+					|| vertical == playerTotal 
+					|| diagonal1 == playerTotal
+					|| diagonal2 == playerTotal) {
 				return true;
 			}
 		}
 		return false;
 	}
 
-	private void setBox(int x, int y, char lastPlayer) {
-		if(board[x-1][y-1] != '\0'){
+	private void setBox(TicTacToeBean bean) {
+		if (board[bean.getX() - 1][bean.getY() - 1] != '\0') {
 			throw new RuntimeException("Box is occupied");
-		}else{
-			board[x-1][y-1] = lastPlayer;
+		} else {
+			board[bean.getX() - 1][bean.getY() - 1] = lastPlayer;
+			getTicTacToeCollection().saveMove(bean);
+			if(!getTicTacToeCollection().saveMove(bean)){
+				throw new RuntimeException("Saving to DB failed");
+			}
 		}
-		
+
 	}
 
 	private void checkAxis(int axis) {
 		if (axis > 3 || axis < 0)
 			throw new RuntimeException("X is exceed their boundaries");
 	}
-	
 
 	public char nextPlayer() {
 		if (lastPlayer == 'X') {
@@ -74,6 +105,7 @@ public class TicTacToe {
 		}
 		return 'X';
 	}
+
 	
 
 }
--- src/test/java/com/ghostl/tddjava/gradle/TicTacToeSpec.java
+++ src/test/java/com/ghostl/tddjava/gradle/TicTacToeSpec.java
@@ -1,13 +1,19 @@
 package com.ghostl.tddjava.gradle;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
+import static org.mockito.Mockito.*;
+
+import com.ghostl.tddjava.beans.TicTacToeBean;
+import com.ghostl.tddjava.mongo.TicTacToeCollection;
 
 public class TicTacToeSpec {
+	
+	private TicTacToeCollection collection;
 
 	@Rule
 	public ExpectedException exception = ExpectedException.none();
@@ -16,7 +22,9 @@ public class TicTacToeSpec {
 	
 	@Before
 	public final void before(){
-		ticTacToe = new TicTacToe();
+		collection = mock(TicTacToeCollection.class);
+		doReturn(true).when(collection).saveMove(any(TicTacToeBean.class));
+		ticTacToe = new TicTacToe(collection);
 	}
 	
 	@Test
@@ -110,6 +118,37 @@ public class TicTacToeSpec {
 		ticTacToe.play(3, 3);//O
 		String actual = ticTacToe.play(3, 2);
 		assertEquals("The result is draw", actual);
+	}
+	
+	@Test
+	public void whenInstantiatedThenSetCollection(){
+		assertNotNull(ticTacToe.getTicTacToeCollection());
+	}
+	
+	@Test
+	public void whenPlayThenSaveMoveIsInvoked(){
+		TicTacToeBean move = new TicTacToeBean(1, 1, 3 , 'X');
+		ticTacToe.play(move.getX(), move.getY());
+		verify(collection).saveMove(move);
+	}
+	
+	@Test
+	public void whenPlayAndSaveReturnsFalseThenThrowException(){
+		doReturn(false).when(collection).saveMove(any(TicTacToeBean.class));
+		TicTacToeBean move = new TicTacToeBean(1, 1, 3, 'X');
+		exception.expect(RuntimeException.class);
+		ticTacToe.play(move.getX(), move.getY());
+		
+	}
+	
+	@Test
+	public void whenPlayInvokedMultipleTimesThenTurnIncreases(){
+		TicTacToeBean move1 = new TicTacToeBean(1, 1, 1, 'X');
+		ticTacToe.play(move1.getX(), move1.getY());
+		verify(collection, times(1)).saveMove(move1);
+		TicTacToeBean move2 = new TicTacToeBean(2, 1, 2, 'O');
+		ticTacToe.play(move2.getX(), move2.getY());
+		verify(collection, times(1)).saveMove(move2);
 	}
 	
 }
